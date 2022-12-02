de1e138e1102d5907e48e5c25704d425f3d081ae
Kate <ktorochkova@gmail.com>
2022-12-02 03:47:05 +0300

    answer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       int t = 1500;
+       int m =  t / 20;
+        System.out.println(m);
     }
 }
